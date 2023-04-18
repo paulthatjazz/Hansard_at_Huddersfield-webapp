17f17165fc0432e4930a1b679a290a338c59a8aa
Alexander von Lünen <avl1@gmx.de>
2023-04-18 22:52:10 +0100

    Fixed bug

--- src/php/db/dbcxn.php
+++ src/php/db/dbcxn.php
@@ -7,9 +7,7 @@
 class DBCxn {
    // What DSN to connect to?
    public static $dsn = 'pgsql:host=localhost;port=5432';
-   public static $driverOpts = array(
-      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-   );
+   public static $driverOpts = NULL;
 
    //turn off search recording, recommended False in test environment
    public static $analytics = True;
